1951bc4640ee46eac4b2d9620f7aaac7496ae3e3
lross2k <luisross2000@outlook.com>
2023-03-21 22:10:34 -0600

    Improvements to the single diode script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# inv-pv-sim
-Code used for PV module simulations
+# PV module simulations
+Code used for PV module simulations, differente mathematical models implemented with the intent of searching the most efficient and precise methods
+
+# Programming language
+The final product will be implemented in ANSI C, but as it is currently in prototyping phase, Lua was chosen as the scripting language, due to it's good interfacing with C and ease of use
+
--- single_diode.lua
+++ single_diode.lua
@@ -1,31 +1,43 @@
--- Constantes fisicas
-q    = 1.6e-19 -- elementary charge
-k    = 1.38e-23 -- Boltzmann constant
+-- Constant values
+q   = 1.6e-19 -- elementary charge
+k   = 1.38e-23 -- Boltzmann constant
+T_r = 25 + 273.15 -- degrees C
 
--- Valores correspondientes al panel
-n_p  = 72
-n_s  = 72 
+-- Datasheet values
+A     = 1.2 -- Ideality factor
+n_p   = 60 -- Amount of PV cells in PV module
+i_sc  = 8.49 -- short circuit current (A)
+alpha = 6 -- mA / C
+T     = 25 + 273.15 -- degrees C
+S     = 1000 -- solar irradiance
+v_oc  = 37.5 -- open circuit voltage of PV module (V) 
 
-A    = 1 -- Ideality factor
-i_sc = 14.04 -- short circuit current (A)
-T_r  = 45 + 273.15 -- degrees C
-alpha = i_sc / 1000 / T_r -- mA / C
-
-T    = 45 + 273.15 -- degrees C
-S    = 1000 -- solar irradiance
-v_oc = 46.73 -- open circuit voltage (V)
---i_rr = math.exp(math.log(i_sc) - v_oc * q / (A * k * T))
-i_rr = 5.39e-5
-
--- current source due to irradiance
-i_ph = S * (i_sc + alpha / 1e3 * (T - T_r))
-E_g = 1.16 - 7.02 * 10 ^ (-4) * (T ^ 2) / (T - 1108)
+-- Calculated values
+v_oc = v_oc / n_p -- open circuit voltage of PV Cell (V) 
+i_rr = math.exp(math.log(i_sc) - v_oc * q / (A * k * T))
+E_g  = 1.16 - 7.02e-4 * (T ^ 2) / (T - 1108)
 i_rs = i_rr * (T / T_r) ^ 3 * math.exp(q * E_g / (k * A) * (1 / T_r - 1 / T))
+n_s  = n_p
+i_ph = (i_sc + alpha * (T - T_r)) * S / 1e3 -- Photovoltaic current source
 
-for v_p=0,50,10 do
-	-- power output
-	p = v_p * n_p * i_ph - v_p * n_s * i_rs * (math.exp(q * v_p / (A * k * T * n_s)) - 1) 
-	print(p)
+-- Array declaration
+i = {}; p = {}; v = {}
+
+-- Generate values for a range of the equation
+index = 0
+for v_p=0,52,v_oc do
+    i_calc = i_ph - i_rs * (math.exp(q * v_p / (A * k * T * n_s)) - 1)
+	if i_calc >= 0 then
+    	i[index] = i_calc
+	    p[index] = i_calc * v_p
+	    v[index] = v_p
+	    index = index + 1
+	else
+	    break
+	end
 end
 
--- https://electricalacademia.com/renewable-energy/photovoltaic-pv-cell-working-characteristics/#:~:text=Iscn%20is%20the%20nominal%20short-circuit%20current%20Ki%20is,ratings%20listed%20for%20commercial%20PV%20cells%20or%20panels.
+-- Print generated values of current and power
+print(table.concat(i,", "))
+print(table.concat(p,", "))
+print(table.concat(v,", "))
